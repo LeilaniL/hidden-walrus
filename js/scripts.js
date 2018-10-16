0f7be0e3b6ed637f0184db7052917718fddac453
Leilani Leach and Stuart Gill <leilani@>
2018-10-16 10:49:06 -0700

    add more jQuery and styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
   $("p").click(function() {
-    $(".walrus-showing").fadeToggle();
+    $(".walrus-showing").slideToggle();
   });
-  // $("p").click(function() {
-  //   $(".walrus-hidden").fadeOut();
-  // });
+  $("button#light").click(function() {
+      $("body").removeClass();
+      $("body").addClass("dark-on-light");
+    });
+
+  $("button#dark").click(function() {
+      $("body").removeClass();
+      $("body").addClass("light-on-dark");
+    });
+    $("p#paragraph").click(function() {
+        $("p#paragraph").toggleClass("highlight");
+      });
 });
